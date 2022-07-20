8c502b74c8a0a08fbdfdf37e21e9e3df6580ae15
Rutger van de Leur <63793669+rutgervandeleur@users.noreply.github.com>
2022-07-20 10:11:24 +0200

    Small update to make leading zeros optional

--- ecg_encrypter.py
+++ ecg_encrypter.py
@@ -9,6 +9,7 @@ from helpers import *
 import hashlib
 from tqdm import tqdm
 
+
 def write_password(filename):
     """
     Generates a key and save it into a file
@@ -17,11 +18,13 @@ def write_password(filename):
     with open(filename, "wb") as key_file:
         key_file.write(key)
 
+
 def load_password(filename):
     """
     Loads the key from the current directory named `key.key`
     """
     return open(filename, "rb").read()
+
 
 def encrypt(filename_input, filename_output, key):
     """
@@ -37,6 +40,7 @@ def encrypt(filename_input, filename_output, key):
     with open(filename_output, "wb") as file:
         file.write(encrypted_data)
 
+
 def remove_specific_xml_field(xml_root, field_path):
     """
     Removes XML field at the specified path in the XML. Works for top-level and
@@ -48,57 +52,68 @@ def remove_specific_xml_field(xml_root, field_path):
             xml_root.find(root_path).remove(xml_root.find(field_path))
         else:
             xml_root.remove(xml_root.find(field_path))
-            
+
     return xml_root
+
 
 def run(config):
     """
     Main loop for pseudonymizing and encrypting all the files.
-    """    
-    
+    """
+
     dir_list = []
-    
+
     print('Reading folder...')
 
-    if config.manufacturer == 'DICOMDIR':  # In case of structured DICOMDIR director(y)(ies)
-            dicomdir_list = []
-            for root, subdirs, files in os.walk(config.in_folder):
-                for file in files:
-                    if file.startswith('DICOMDIR'):
-                        dicomdir_list.append(os.path.join(root, file))
+    # In case of structured DICOMDIR director(y)(ies)
+    if config.manufacturer == 'DICOMDIR':
+        dicomdir_list = []
+        for root, subdirs, files in os.walk(config.in_folder):
+            for file in files:
+                if file.startswith('DICOMDIR'):
+                    dicomdir_list.append(os.path.join(root, file))
 
-            for dicom_dir_file in dicomdir_list:
-                dicom_dir = dicom.filereader.read_dicomdir(dicom_dir_file)
-                base_dir = os.path.dirname(dicom_dir_file)
+        for dicom_dir_file in dicomdir_list:
+            dicom_dir = dicom.filereader.read_dicomdir(dicom_dir_file)
+            base_dir = os.path.dirname(dicom_dir_file)
 
-                for patient_record in dicom_dir.patient_records:
-                    for study in patient_record.children:
-                        for series in study.children:
-                            image_records = series.children
-                            image_filenames = [os.path.join(base_dir, *image_rec.ReferencedFileID)
-                                            for image_rec in image_records]
-                            for item in image_filenames:
-                                dir_list.append(item)
-    
-    else: 
+            for patient_record in dicom_dir.patient_records:
+                for study in patient_record.children:
+                    for series in study.children:
+                        image_records = series.children
+                        image_filenames = [os.path.join(base_dir, *image_rec.ReferencedFileID)
+                                           for image_rec in image_records]
+                        for item in image_filenames:
+                            dir_list.append(item)
+
+    else:
         for root, subdirs, files in tqdm(os.walk(config.in_folder)):
             for file in files:
                 if (file.endswith('.xml') or file.endswith('.XML')):
                     dir_list.append(os.path.join(root, file))
-                elif (file.startswith('1.2.840.113654.2.70.1') or 
-                    file.endswith('.dcm') or file.endswith('.DCM')):
+                elif (file.startswith('1.2.840.113654.2.70.1') or
+                      file.endswith('.dcm') or file.endswith('.DCM')):
                     ds = dicom.read_file(os.path.join(root, file))
                     if ds.SOPClassUID == '1.2.840.10008.5.1.4.1.1.9.1.1':
                         dir_list.append(os.path.join(root, file))
 
     print(f'{str(len(dir_list))} files found in this folder, is that correct?')
 
-    keys = pd.read_csv(config.key, sep=None, engine='python', encoding='utf-8-sig')
+    keys = pd.read_csv(config.key, sep=None,
+                       engine='python', encoding='utf-8-sig')
     if not keys['PID'].dtypes == str:
         patient_id_length = keys['PID'].astype(str).map(len).max()
         reformat_string = '{:0>' + str(patient_id_length) + '}'
-        print(f'Adding leading zeros to numeric PatientID of length {str(patient_id_length)}, is that correct?')
-        keys['PID'] = keys['PID'].astype(int).astype(str).apply(reformat_string.format)
+        leading_zeros = input(
+            f'Do you want to add leading zeros to numeric PatientID of length {str(patient_id_length)}? [Y/n]')
+
+        if leading_zeros in ["", "Y"]:
+            keys['PID'] = keys['PID'].astype(int).astype(
+                str).apply(reformat_string.format)
+        elif leading_zeros == "n":
+            pass
+        else:
+            raise AssertionError("Please respond with Y or n...")
     keys['PseudoID'] = keys['PseudoID'].astype(str)
 
     if not os.path.exists(os.path.join(config.in_folder, 'password.key')):
@@ -108,10 +123,10 @@ def run(config):
         print('Using current password...')
 
     password = load_password(os.path.join(config.in_folder, 'password.key'))
-    
+
     print('Starting pseudonymisation and encryption...')
 
-    for idx, path in tqdm(enumerate(dir_list)):    
+    for idx, path in tqdm(enumerate(dir_list)):
         if config.manufacturer == 'MUSE':
             xml_tree = ET.parse(path)
             xml_root = xml_tree.getroot()
@@ -124,15 +139,15 @@ def run(config):
             timestamp = acq_date + acq_time
 
             # Remove specific fields
-            remove_field = ['PatientDemographics', 'Order', 
-                'TestDemographics/OverreaderID', 
-                'TestDemographics/EditorID', 
-                'TestDemographics/OverreaderLastName', 
-                'TestDemographics/OverreaderFirstName', 
-                'TestDemographics/EditorLastName', 
-                'TestDemographics/EditorFirstName', 
-                'TestDemographics/SecondaryID'
-            ]
+            remove_field = ['PatientDemographics', 'Order',
+                            'TestDemographics/OverreaderID',
+                            'TestDemographics/EditorID',
+                            'TestDemographics/OverreaderLastName',
+                            'TestDemographics/OverreaderFirstName',
+                            'TestDemographics/EditorLastName',
+                            'TestDemographics/EditorFirstName',
+                            'TestDemographics/SecondaryID'
+                            ]
 
             for field_path in remove_field:
                 xml_root = remove_specific_xml_field(xml_root, field_path)
@@ -170,44 +185,51 @@ def run(config):
                 # Remove PatientDemograpics
                 ds.PatientName = ''
                 ds.PatientID = ''
-                ds.PatientBirthDate = ''          
+                ds.PatientBirthDate = ''
 
         # Define new filename
-        if patient_id in keys['PID'].values:
-            pseudo_id = keys.loc[keys['PID'] == patient_id, 'PseudoID'].values[0]
+        if keys['PID'].isin([patient_id]).sum() > 0:
+            pseudo_id = keys.loc[keys['PID'] ==
+                                 patient_id, 'PseudoID'].values[0]
         else:
-            print(f'The XML/DCM file with name {path} has no associated RedCap ID, please check.')
-            print(patient_id)
-            print(keys['PID'])
+            print(
+                f'The XML/DCM file with name {path} has no associated RedCap ID, please check.')
             continue
-        
-        timestamp_hash = hashlib.sha256(timestamp.encode('utf-8')).hexdigest()[-10:]
+
+        timestamp_hash = hashlib.sha256(
+            timestamp.encode('utf-8')).hexdigest()[-10:]
         file_out = str(pseudo_id) + '_' + timestamp_hash
 
         # Save pseudonimized and encrypted files
         if config.manufacturer in ['MUSE', 'MORTARA']:
             xml_tree.write(os.path.join(config.out_folder, file_out + '.xml'))
-            encrypt(os.path.join(config.out_folder, file_out + '.xml'), 
-            os.path.join(config.encrypted_folder, file_out + '.enc'), password)
+            encrypt(os.path.join(config.out_folder, file_out + '.xml'),
+                    os.path.join(config.encrypted_folder, file_out + '.enc'), password)
         elif config.manufacturer in ['DICOM', 'DICOMDIR']:
             ds.save_as(os.path.join(config.out_folder, file_out + '.dcm'))
-            encrypt(os.path.join(config.out_folder, file_out + '.dcm'), 
-            os.path.join(config.encrypted_folder, file_out + '.enc'), password)
-    
+            encrypt(os.path.join(config.out_folder, file_out + '.dcm'),
+                    os.path.join(config.encrypted_folder, file_out + '.enc'), password)
+
     print(f'Done, processed {str(idx+1)} files!')
 
     return idx+1
+
 
 if __name__ == "__main__":
     # Parser
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--in_folder', type=str, default="test_dir", help="Path to folder with xml or dicom waveform files")
-    parser.add_argument('--out_folder', type=str, default="out_test_dir", help="Folder name to put converted files in")
-    parser.add_argument('--encrypted_folder', type=str, default="encrypt_test_dir", help="Folder name to put encrypted files in")
-    parser.add_argument('--key', type=str, default="key.csv", help="CSV file with local PIDs (named 'PID') and pseudonyms (named 'PseudoID')")
-    parser.add_argument('--manufacturer', type=str, default="MUSE", help="Enter manufacturer, options: MUSE or MORTARA or DICOM or DICOMDIR")
-    
+    parser.add_argument('--in_folder', type=str, default="test_dir",
+                        help="Path to folder with xml or dicom waveform files")
+    parser.add_argument('--out_folder', type=str, default="out_test_dir",
+                        help="Folder name to put converted files in")
+    parser.add_argument('--encrypted_folder', type=str, default="encrypt_test_dir",
+                        help="Folder name to put encrypted files in")
+    parser.add_argument('--key', type=str, default="key.csv",
+                        help="CSV file with local PIDs (named 'PID') and pseudonyms (named 'PseudoID')")
+    parser.add_argument('--manufacturer', type=str, default="MUSE",
+                        help="Enter manufacturer, options: MUSE or MORTARA or DICOM or DICOMDIR")
+
     config = parser.parse_args()
 
     assert(config.manufacturer in ['MUSE', 'MORTARA', 'DICOM', 'DICOMDIR'])
